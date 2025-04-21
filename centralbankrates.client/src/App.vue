910f313d03b8f57080814e627ea0188518cb2fb2
sedov <sedov.david@yandex.ru>
2025-04-21 21:36:59 +0300

    bugfix

--- centralbankrates.client/src/App.vue
+++ centralbankrates.client/src/App.vue
@@ -1,15 +1,11 @@
 <script setup>
 import CurrenciesTable from './components/CurrenciesTable.vue'
-import HelloWorld from "@/components/HelloWorld.vue";
 import Filter from "@/components/Filter.vue";
 </script>
 
 <template>
-<!--  <input v-model="message" placeholder="edit me" />-->
-
   <Filter/>
   <CurrenciesTable/>
-<!--  <HelloWorld/>-->
 
 </template>
 
